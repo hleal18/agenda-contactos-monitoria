105c21b5ab595b43d8535d6b9f565aa267ab382b
Humberto Leal <humberto.adolfo@hotmail.com>
2019-02-13 03:41:24 -0500

    Pruebita de escritorio sobre comportamiento de clase

--- Agenda/src/vista/VentanaPrincipal.java
+++ Agenda/src/vista/VentanaPrincipal.java
@@ -5,6 +5,8 @@
  */
 package vista;
 
+import logica.Agenda;
+import logica.Contacto;
 /**
  *
  * @author usuario
@@ -16,6 +18,45 @@ public class VentanaPrincipal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Agenda cuadernito = new Agenda(3);
+        System.out.println("Inicio");
+        Contacto padre = new Contacto("123", "Humberto", "Leal", "3012511943", "Las Gaviotas Mz34 Lte15");
+        Contacto hijo = new Contacto("122", "Gregorio", "Leal", "3103265151", "Las Gaviotas Mz 34 Lte15");
+        Contacto madrecita = new Contacto("124", "Mariana", "Gutierrez", "303123456", "bocagrande");
+        
+        if (cuadernito.agregar(padre))
+            System.out.println("Se agregó el usuario: " + padre.toString());
+        else
+            System.out.println("Hubo un error agregando a: " + padre.toString());
+        
+        if (cuadernito.agregar(hijo))
+            System.out.println("Se agregó el usuario: " + hijo.toString());
+        else
+            System.out.println("Hubo un error agregando a: " + hijo.toString());
+        
+        if (cuadernito.agregar(madrecita))
+            System.out.println("Se agregó el usuario: " + madrecita.toString());
+        else
+            System.out.println("Hubo un error agregando a: " + madrecita.toString());
+        
+        if (!cuadernito.agregar(hijo))
+            System.out.println("No se dejó agregar, bien!");
+        else
+            System.out.println("Se dejó agregar, Mal!");
+        
+        System.out.println("Buscando a mi hijo:");
+        if (cuadernito.buscar(hijo.getId()) != null)
+            System.out.println("Hijo encontrado");
+        
+        if (cuadernito.modificar(new Contacto("123", "", "", "", "cuchicuchi")))
+            System.out.println("Padre modificado: " + padre.toString());
+        
+        if (cuadernito.eliminar(padre.getId()))
+            System.out.println("padre eliminado");
+        
+        System.out.println("Buscando al hijo.");
+        if (cuadernito.buscar(hijo.getId()) != null)
+            System.out.println("Hijo encontrado");
     }
     
 }
